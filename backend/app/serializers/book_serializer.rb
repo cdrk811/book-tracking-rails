b68ce6f94d7bafc23b8fef9d19a2e0251c75fa3d
cedrickvalenciaa <cedrickvalencia811@gmail.com>
2025-02-24 22:11:35 +0800

    Feature: add api controller and book controller

---
+++ backend/app/serializers/book_serializer.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+class BookSerializer < ActiveModel::Serializer
+  attributes :id, :title, :description, :book_image, :author_avatar, :author_name, :pages,
+             :categories, :status, :updated_at
+
+  def updated_at
+    object.updated_at.strftime('%Y-%m-%d %H:%M:%S')
+  end
+
+  def categories
+    object.categories.pluck(:name)
+  end
+
+  def status
+    object.status
+  end
+end
